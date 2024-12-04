51648452031283edb881d678b04d932583f31731
Gabe Martens <gabrielrobertmartens@gmail.com>
2024-12-04 17:55:14 -0600

    add all 3 bluetooth chars

--- avr/ble.X/avr-main.c
+++ avr/ble.X/avr-main.c
@@ -5,6 +5,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "usart.h"
 
@@ -56,27 +57,31 @@ void bleInit(const char *name) {
 
     // Add a new service. Feel free to use any ID you want from the
     // BLE assigned numbers document. Avoid the "generic" services.
-    usart0WriteCommand("PS,1812\r\n");
+    usart0WriteCommand("PS,1815\r\n");
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
 
     // Add a new characteristic to the service for your accelerometer
     // data. Pick any ID you want from the BLE assigned numbers document.
     // Avoid the "generic" characteristics.
-    usart0WriteCommand("PC,2BE2,0A,04\r\n");
+    usart0WriteCommand("PC,2BE1,0A,04\r\n"); // 72
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
     
-    usart0WriteCommand("PC,2BE2,0A,04\r\n");
+    usart0WriteCommand("PC,2BE2,0A,04\r\n"); // 74
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
     
-    usart0WriteCommand("PC,2BE2,0A,04\r\n");
+    usart0WriteCommand("PC,2BE3,0A,04\r\n"); // 76
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
 
-    // Set the characteristic's initial value to hex "00".
-    usart0WriteCommand("LS\r\n");
+    usart0WriteCommand("LS\r\n"); // list
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
     
-    
     usart0WriteCommand("SHW,0072,00\r\n");
+    usart0ReadUntil(buf, BLE_RADIO_PROMPT);
+    
+    usart0WriteCommand("SHW,0074,00\r\n");
+    usart0ReadUntil(buf, BLE_RADIO_PROMPT);
+    
+    usart0WriteCommand("SHW,0076,00\r\n");
     usart0ReadUntil(buf, BLE_RADIO_PROMPT);
     
 }
@@ -86,11 +91,47 @@ int main() {
     usart1Init(9600);
     
     char buf[BUF_SIZE];
+    uint8_t faders[3] = {0, 0, 0};
+    uint8_t faders_old[3] = {0, 0, 0};
     while (1) {
         usart0WriteCommand("SHR,0072\r\n");
         usart0ReadUntil(buf, BLE_RADIO_PROMPT);
+        if(strncmp(buf+2, "\r\nCMD", 5) == 0) {
+            memset(buf+2, 0, BUF_SIZE-2);
+            faders[0] = strtoul(buf, NULL, 16);
+        }
         
-        usart1WriteCommand(buf);
-        _delay_ms(1000);
+        usart0WriteCommand("SHR,0074\r\n");
+        usart0ReadUntil(buf, BLE_RADIO_PROMPT);
+        if(strncmp(buf+2, "\r\nCMD", 5) == 0) {
+            memset(buf+2, 0, BUF_SIZE-2);
+            faders[1] = strtoul(buf, NULL, 16);
+        }
+        
+        usart0WriteCommand("SHR,0076\r\n");
+        usart0ReadUntil(buf, BLE_RADIO_PROMPT);
+        if(strncmp(buf+2, "\r\nCMD", 5) == 0) {
+            memset(buf+2, 0, BUF_SIZE-2);
+            faders[2] = strtoul(buf, NULL, 16);
+        }
+        
+        if(faders[0] != faders_old[0]) {
+            snprintf(buf, BUF_SIZE, "F0,%03d\n", faders[0]);
+            usart1WriteCommand(buf);
+        }
+        if(faders[1] != faders_old[1]) {
+            snprintf(buf, BUF_SIZE, "F1,%03d\n", faders[1]);
+            usart1WriteCommand(buf);
+        }
+        if(faders[2] != faders_old[2]) {
+            snprintf(buf, BUF_SIZE, "F2,%03d\n", faders[2]);
+            usart1WriteCommand(buf);
+        }
+        
+        faders_old[0] = faders[0];
+        faders_old[1] = faders[1];
+        faders_old[2] = faders[2];
+        
+        _delay_ms(100);
     }
 }
